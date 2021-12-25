51f48e4850e940da6bc4015d7dd50d22c8ca35e5
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-25 11:31:29 +0900

    beforeフィルタによるDBコネクション関連の実行および重複コードを排除

--- main.rb
+++ main.rb
@@ -12,11 +12,15 @@ class MemoManager
   CONNECTION = PG.connect(dbname: DB_NAME)
 
   def self.prepared_connection
-    CONNECTION.prepare('all', "SELECT * FROM #{TABLE_NAME} ORDER BY id") unless MemoManager.prepared_exist?('all')
-    CONNECTION.prepare('select', "SELECT * FROM #{TABLE_NAME} WHERE id = $1") unless MemoManager.prepared_exist?('select')
-    CONNECTION.prepare('create', "INSERT INTO #{TABLE_NAME} (title, content) values ($1, $2)") unless MemoManager.prepared_exist?('create')
-    CONNECTION.prepare('update', "UPDATE #{TABLE_NAME} SET title = $1, content = $2 WHERE id = $3") unless MemoManager.prepared_exist?('update')
-    CONNECTION.prepare('delete', "DELETE FROM #{TABLE_NAME} WHERE id = $1") unless MemoManager.prepared_exist?('delete')
+    [
+      ['all', "SELECT * FROM #{TABLE_NAME} ORDER BY id"],
+      ['select', "SELECT * FROM #{TABLE_NAME} WHERE id = $1"],
+      ['create', "INSERT INTO #{TABLE_NAME} (title, content) values ($1, $2)"],
+      ['update', "UPDATE #{TABLE_NAME} SET title = $1, content = $2 WHERE id = $3"],
+      ['delete', "DELETE FROM #{TABLE_NAME} WHERE id = $1"]
+    ].each do |name, query|
+      CONNECTION.prepare(name, query) unless prepared_exist?(name)
+    end
     MemoManager.new
   end
 
@@ -58,10 +62,13 @@ not_found do
   erb :error_not_found
 end
 
+before do
+  @memo_manager = MemoManager.prepared_connection
+end
+
 # GET / => トップページ
 get '/' do
-  memo_manager = MemoManager.prepared_connection
-  @memos = memo_manager.all
+  @memos = @memo_manager.all
   erb :index
 end
 
@@ -72,35 +79,30 @@ end
 
 # POST /memos => 新規作成する
 post '/memos' do
-  memo_manager = MemoManager.prepared_connection
-  memo_manager.create(params[:title], params[:content])
+  @memo_manager.create(params[:title], params[:content])
   redirect to('/')
 end
 
 # GET /memos/1 => 詳細ページ
 get '/memos/:id' do
-  memo_manager = MemoManager.prepared_connection
-  @memo = memo_manager.find(params[:id])
+  @memo = @memo_manager.find(params[:id])
   erb :show
 end
 
 # GET /memos/1/edit => 編集ページ
 get '/memos/:id/edit' do
-  memo_manager = MemoManager.prepared_connection
-  @memo = memo_manager.find(params[:id])
+  @memo = @memo_manager.find(params[:id])
   erb :edit
 end
 
 # PATCH /memos/1 => 更新する
 patch '/memos/:id' do
-  memo_manager = MemoManager.prepared_connection
-  memo_manager.update(params[:id], params[:title], params[:content])
+  @memo_manager.update(params[:id], params[:title], params[:content])
   redirect to('/')
 end
 
 # DELETE /memos/1 => 削除する
 delete '/memos/:id' do
-  memo_manager = MemoManager.prepared_connection
-  memo_manager.destroy(params[:id])
+  @memo_manager.destroy(params[:id])
   redirect to('/')
 end
